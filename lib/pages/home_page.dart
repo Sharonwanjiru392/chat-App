d2dfcbd6ef865e5fb5d5d1eee209aa4919840897
Sharon Wanjiru <sharonwanjiru392@gmail.com>
2025-05-14 09:30:12 +0300

    am done with the menu drawer and yeah

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,27 +1,20 @@
-import 'package:chat_app/auth/auth_service.dart';
+import 'package:chat_app/components/my_drawer.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
   //logout method
-  void logout() {
-    //get auth service
-    final _auth = AuthService();
-    _auth.signOut();
-  }
+ 
 
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text("home page"),
-      actions: [
-        //logout button
-        IconButton(onPressed: logout, icon: Icon(Icons.logout)),
-      ],
+      
       ),
-      drawer: Drawer(),
+      drawer: MyDrawer(),
     );
   }
 }

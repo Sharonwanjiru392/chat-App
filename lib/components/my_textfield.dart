793149d9ef2a43da7019c807f202e98a870ff2b4
Sharon Wanjiru <sharonwanjiru392@gmail.com>
2025-05-16 09:26:37 +0300

    am done with that now maybe the admin

--- lib/components/my_textfield.dart
+++ lib/components/my_textfield.dart
@@ -4,11 +4,13 @@ class MyTextfield extends StatelessWidget {
   final String hintText;
   final bool obscureText;
   final TextEditingController? controller;
+  final FocusNode? focusNode;
   const MyTextfield({
     super.key,
     required this.hintText,
     required this.obscureText,
     required this.controller,
+    this.focusNode,
     });
 
   @override
@@ -18,6 +20,7 @@ class MyTextfield extends StatelessWidget {
       child: TextField(
         obscureText: obscureText,
         controller: controller,
+        focusNode: focusNode,
         decoration: InputDecoration(
           enabledBorder: OutlineInputBorder(
             borderSide: BorderSide(color: Theme.of(context).colorScheme.primary),
--- lib/pages/chat_page.dart
+++ lib/pages/chat_page.dart
@@ -4,34 +4,78 @@ import 'package:chat_app/services/chats/chat_service.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
-class ChatPage extends StatelessWidget {
+class ChatPage extends StatefulWidget {
   final String receiverEmail;
   final String receiverID;
   ChatPage({super.key, required this.receiverEmail, required this.receiverID});
 
+  @override
+  State<ChatPage> createState() => _ChatPageState();
+}
+
+class _ChatPageState extends State<ChatPage> {
   //text controller for message input
   final TextEditingController _messageController = TextEditingController();
 
   //chat and auth services
   final ChatService _chatService = ChatService();
+
   final AuthService _authService = AuthService();
+
+  //for text field focus
+  FocusNode myFocusNode = FocusNode();
+  @override
+  void initState() {
+    super.initState();
+    //add listener to focus node
+    myFocusNode.addListener(() {
+      if (myFocusNode.hasFocus) {
+        //cause a delay so as the keyboard has time to show up
+
+        //then the amount of the remaining space will be calculated
+
+        //then scroll down
+        Future.delayed(const Duration(milliseconds: 400), () => scrollDown());
+      }
+    });
+    //wait a bit for listView to be build  to bottom
+    Future.delayed(const Duration(milliseconds: 100), () => 
+    scrollDown());
+  }
+  @override
+  void dispose() {
+    myFocusNode.dispose();
+    _messageController.dispose();
+    super.dispose();
+  }
+  //scroll controler
+  final ScrollController _scrollController = ScrollController();
+
+  void scrollDown() {
+    //scroll to the bottom of the list
+    _scrollController.animateTo(_scrollController.position.maxScrollExtent, 
+    duration: const Duration(seconds: 1), 
+    curve: Curves.fastOutSlowIn);
+  }
 
   //send message method
   void sendMessage() async {
     //if there is something inside the message text field
     if (_messageController.text.isNotEmpty) {
       //send the message
-      await _chatService.sendMessage(receiverID, _messageController.text);
+      await _chatService.sendMessage(widget.receiverID, _messageController.text);
 
       //clear the text field
       _messageController.clear();
     }
+    scrollDown();
+
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text(receiverEmail),
+      appBar: AppBar(title: Text(widget.receiverEmail),
       backgroundColor: Colors.transparent,
       foregroundColor: Colors.grey,
       elevation: 0,
@@ -54,7 +98,7 @@ class ChatPage extends StatelessWidget {
     String senderID = _authService.getCurrentUser()!.uid;
 
     return StreamBuilder(
-      stream: _chatService.getMessages(receiverID, senderID),
+      stream: _chatService.getMessages(widget.receiverID, senderID),
       builder: (context, snapshot) {
         //error
         if (snapshot.hasError) {
@@ -66,6 +110,7 @@ class ChatPage extends StatelessWidget {
         }
         //return list view
         return ListView(
+          controller: _scrollController,
           children:
               snapshot.data!.docs.map((doc) => _buildMessageItem(doc)).toList(),
         );
@@ -107,6 +152,7 @@ class ChatPage extends StatelessWidget {
               hintText: "Type a message",
             ),
             obscureText: false,
+            focusNode: myFocusNode,
           ),
         ),
         //send button
